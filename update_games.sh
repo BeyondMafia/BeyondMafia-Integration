48b1c2db74d339be4bef74549ebc42b8fc1d9aca
r3ndd <3lliotn98@gmail.com>
2023-01-04 15:49:31 -0700

    Games update script changes

--- update_games.sh
+++ update_games.sh
@@ -3,6 +3,6 @@ git pull;
 rm -rf react_main/build_public;
 cp -r react_main/build react_main/build_public;
 
-pm2 scale games +1;
+pm2 scale games +1 --no-autorestart;
 redis-cli publish deprecate $1;
 pm2 restart www;
