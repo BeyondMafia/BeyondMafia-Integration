619f87da89c8722c76a6317b084bc04cb4ce4bff
r3ndd <3lliotn98@gmail.com>
2023-01-05 10:31:53 -0700

    React build

--- react_main/build/precache-manifest.840e3efc04f05d6a94c75817116e58e9.js
+++ react_main/build/precache-manifest.840e3efc04f05d6a94c75817116e58e9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3eb7c2d1548338818fa57a2704962e43",
+    "revision": "88487f52c62ba70230133cf896caaea0",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.93c90398.chunk.css"
   },
   {
-    "revision": "bdbcf7e9b0d7a6e520b3",
+    "revision": "ae9297af3fcfcb244df1",
     "url": "/static/css/main.926afc6c.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ebe49947.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bdbcf7e9b0d7a6e520b3",
-    "url": "/static/js/main.0d9fb29e.chunk.js"
+    "revision": "ae9297af3fcfcb244df1",
+    "url": "/static/js/main.a04ee909.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/static/js/main.0d9fb29e.chunk.js.LICENSE.txt"
+    "url": "/static/js/main.a04ee909.chunk.js.LICENSE.txt"
   },
   {
     "revision": "15c7af0c929ff2659c72",
--- react_main/build/service-worker.js
+++ react_main/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0532262a823194bc9c03cceaf4ff5a09.js"
+  "/precache-manifest.840e3efc04f05d6a94c75817116e58e9.js"
 );
 
 self.addEventListener('message', (event) => {
