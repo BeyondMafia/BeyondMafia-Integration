14bb912c1074644d96c5325a64337b2af614cc81
Shwartz99 <83051898+Shwartz99@users.noreply.github.com>
2023-01-08 02:18:11 -0500

    fixed insane cop
    thanks travis

--- Games/types/Mafia/roles/cards/AlignmentLearnerReversed.js
+++ Games/types/Mafia/roles/cards/AlignmentLearnerReversed.js
@@ -17,9 +17,9 @@ module.exports = class AlignmentLearnerReversed extends Card {
 						var alignment = this.game.getRoleAlignment(role);
 
 						if (alignment == "Village" || alignment == "Independent")
-							alignment = "the Mafia"
+							alignment = "Mafia"
 						else if (alignment == "Mafia" || alignment == "Monsters")
-							alignment = "the Village";
+							alignment = "Village";
 						
 						var alert = `You learn that ${this.target.name} is sided with the ${alignment}.`;
 						this.game.queueAlert(alert, 0, this.meeting.getPlayers());
