2e0cd3ee058900692e8073ab342d03b329215042
R H <raycxhuang@gmail.com>
2023-01-09 21:14:51 +1100

    Update and rename AppearAsVillageIdiot.js to AppearAsVillage.js

--- Games/types/Mafia/roles/cards/AppearAsVillage.js
+++ Games/types/Mafia/roles/cards/AppearAsVillage.js
@@ -1,6 +1,6 @@
 const Card = require("../../Card");
 
-module.exports = class AppearAsVillageIdiot extends Card {
+module.exports = class AppearAsVillage extends Card {
 
 	constructor(role) {
 		super(role);
