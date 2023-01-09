1695add03a1a6bdd37554ae9203af5271b907f22
R H <raycxhuang@gmail.com>
2023-01-09 13:32:54 +1100

    Update and rename NightSaveBlock.js to NightNurse.js

--- Games/types/Mafia/roles/cards/NightNurse.js
+++ Games/types/Mafia/roles/cards/NightNurse.js
@@ -1,20 +1,20 @@
 const Card = require("../../Card");
-const { PRIORITY_NIGHT_SAVEBLOCK } = require("../../const/Priority");
+const { PRIORITY_NIGHT_NURSE } = require("../../const/Priority");
 
-module.exports = class NightSaveBlock extends Card {
+module.exports = class NightNurse extends Card {
 
 	constructor(role) {
 		super(role);
 
 		this.meetings = {
-			"Save": {
+			"Nurse": {
 				states: ["Night"],
 				flags: ["voting"],
 				action: {
 					labels: ["save", "block"],
-					priority: PRIORITY_NIGHT_SAVEBLOCK,
+					priority: PRIORITY_NIGHT_NURSE,
 					run: function () {
-            for (let action of this.game.actions[0]) {
+						for (let action of this.game.actions[0]) {
 							if (
 								action.actor == this.target &&
 								action.priority > this.priority &&
