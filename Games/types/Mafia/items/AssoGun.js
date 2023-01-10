0486cf64d53f695939617636ecf4ec6d7721055d
DrSharky <keleblanc24@gmail.com>
2023-01-09 21:10:24 -0500

    Update 50% chance to reveal

--- Games/types/Mafia/items/AssoGun.js
+++ Games/types/Mafia/items/AssoGun.js
@@ -3,9 +3,10 @@ const Random = require("../../../../lib/Random");
 
 module.exports = class AssoGun extends Item {
 
-    constructor() {
+    constructor(reveal) {
         super("AssoGun");
 
+        this.reveal = reveal;
         this.meetings = {
             "Shoot Gun": {
                 actionName: "Shoot",
@@ -15,7 +16,15 @@ module.exports = class AssoGun extends Item {
                     labels: ["kill", "gun"],
                     item: this,
                     run: function () {
-                        this.game.queueAlert(`${this.actor.name} pulls a gun and shoots at ${this.target.name}!`);
+                        var reveal = this.item.reveal;
+
+                        if (reveal === null)
+                            reveal = Random.randArrayVal([true, false]);
+
+                        if (reveal)
+                            this.game.queueAlert(`${this.actor.name} pulls a gun and shoots at ${this.target.name}!`);
+                        else
+                            this.game.queueAlert(`Someone fires a gun at ${this.target.name}!`);
 
                         if (this.dominates()) {
                             if (this.target.role.alignment !== "Mafia") {
