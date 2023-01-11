b6046f4d272aaea1f9d40117f70edcc9e6b59e10
gameshowmaster <gameshowmaster@gmail.com>
2023-01-10 23:43:37 -0500

    first draft of arsonist

---
+++ Games/types/Mafia/roles/cards/DouseInGasoline.js
@@ -0,0 +1,25 @@
+const Card = require("../../Card");
+const { PRIORITY_ARSONIST } = require("../../const/Priority");
+
+module.exports = class DouseInGasoline extends Card {
+
+	constructor(role) {
+		super(role);
+
+		this.meetings = {
+			"Douse Villager": {
+				states: ["Night"],
+				flags: ["voting"],
+				targets: { include: ["alive"], exclude: ["Mafia"] },
+				action: {
+					labels: ["giveItem", "gasoline"],
+					priority: PRIORITY_ARSONIST,
+					run: function () {
+						this.target.holdItem("Gasoline");
+					}
+				}
+			}
+		};
+	}
+
+}
