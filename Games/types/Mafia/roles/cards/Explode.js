ac3073072587ad4ad1551d16f5d7c3df5021932c
R H <raycxhuang@gmail.com>
2023-01-12 11:04:52 +1100

    Create Explode.js

---
+++ Games/types/Mafia/roles/cards/Explode.js
@@ -0,0 +1,27 @@
+const Card = require("../../Card");
+const { PRIORITY_EXPLODE } = require("../../const/Priority");
+
+module.exports = class Explode extends Card {
+
+	constructor(role) {
+		super(role);
+
+		this.meetings = {
+			"Explode": {
+				states: ["Day"],
+				flags: ["voting"],
+				action: {
+					labels: ["kill"],
+					priority: PRIORITY_EXPLODE,
+					run: function () {
+            this.game.queueAlert(`${this.actor.name} rushes at ${this.target.name} and reveals a bomb!`);
+						if (this.dominates())
+							this.target.kill("basic", this.actor);
+              this.actor.kill("basic", this.actor);
+					}
+				}
+			}
+		};
+	}
+
+}
