ff24f89faae12c96a9e87b603bdb5a4260b7c3d7
r3ndd <3lliotn98@gmail.com>
2023-01-12 19:35:15 -0700

    Caroler fix

--- Games/types/Mafia/roles/cards/Carol.js
+++ Games/types/Mafia/roles/cards/Carol.js
@@ -15,44 +15,38 @@ module.exports = class Carol extends Card {
 					labels: ["carol"],
 					priority: PRIORITY_CAROL,
 					run: function () {
+						var prevTarget = this.actor.role.data.prevTarget;
+						this.actor.role.data.prevTarget = this.target;
+
+						if (this.target == prevTarget)
+							return;
+
+						for (let action of this.game.actions[0])
+							if (action.actor == this.target && !action.hasLabel("hidden"))
+								return;
+
 						var carol;
 						var alive = this.game.players.filter(p => p.alive);
+						var mafia = alive.filter(p => p.role.alignment == "Mafia");
 						var chosenThree = [
 							Random.randArrayVal(alive, true),
 							Random.randArrayVal(alive, true),
 							Random.randArrayVal(alive, true)
 						];
 
-						if (chosenThree.filter(p => p.role.alignment == "Mafia").length == 0) {
-
-						}
-
-						if (mafia.length > 0) {
-							chosenThree.push(Random.randArrayVal(mafia));
-							chosenThree = Random.randomizeArray(chosenThree);
-
-							carol = "You see a merry Caroler outside your house! \
-								They sing you a Carol about " + chosenThree[0].name + ", " + chosenThree[1].name + ", " + chosenThree[2].name + ",\
-								at least one of whom is the Mafia!";
+						if (mafia.length == 0) {
+							carol = `You see a merry Caroler outside your house! They sing you a happy song about all of the Mafia being dead!`;
 						}
 						else {
-							carol = `You see a merry Caroler outside your house! \
-								They sing you a happy song about all of the Mafia being dead!`;
-						}
-
-						var targetVisits = false;
-
-						for (let action of this.game.actions[0]) {
-							if (action.actor == this.target && !action.hasLabel("hidden")) {
-								targetVisits = true;
-								break;
+							if (chosenThree.filter(p => p.role.alignment == "Mafia").length == 0) {
+								chosenThree[0] = Random.randArrayVal(mafia);
+								chosenThree = Random.randomizeArray(chosenThree);
 							}
+
+							carol = `You see a merry Caroler outside your house! They sing you a Carol about ${chosenThree[0].name}, ${chosenThree[1].name}, ${chosenThree[2].name}, at least one of whom is the Mafia!`;
 						}
 
-						if (!targetVisits && this.target != this.actor.role.data.prevTarget)
-							this.target.queueAlert(carol);
-
-						this.actor.role.data.prevTarget = this.target;
+						this.target.queueAlert(carol);
 					}
 				}
 			}
