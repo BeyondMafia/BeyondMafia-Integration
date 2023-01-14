5ac53ad168f731e41b6ce5a724d3c282ebbe20be
R H <raycxhuang@gmail.com>
2023-01-14 12:20:29 +1100

    Create Illusionist.js

---
+++ Games/types/Mafia/roles/Mafia/Illusionist.js
@@ -0,0 +1,11 @@
+const Role = require("../../Role");
+
+module.exports = class Illusionist extends Role {
+
+	constructor(player, data) {
+		super("Illusionist", player, data);
+		this.alignment = "Mafia";
+		this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "StartWithIlluGun"];
+	}
+
+}
