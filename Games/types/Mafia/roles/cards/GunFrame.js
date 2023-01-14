bb0958fcc16c331f7357d5a1914578be03d585ed
R H <raycxhuang@gmail.com>
2023-01-14 12:29:07 +1100

    Update and rename StartWithIlluGun.js to GunFrame.js

---
+++ Games/types/Mafia/roles/cards/GunFrame.js
@@ -0,0 +1,26 @@
+const Card = require("../../Card");
+const { PRIORITY_GUN_FRAME } = require("../../const/Priority");
+
+module.exports = class IlluGunFrame extends Card {
+
+	constructor(role) {
+		super(role);
+		
+		this.startItems = ["IlluGun"];
+		
+		this.meetings = {
+			"Frame Shooter": {
+				actionName: "Frame as Shooter",
+				states: ["Night"],
+				flags: ["voting"],
+				action: {
+					priority: PRIORITY_GUN_FRAME,
+					run: function () {
+						//…
+					}
+				}
+			}
+		};
+	}
+
+}
