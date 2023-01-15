adf5a6c39e52c447e227fcd2aa502e0d1313d0e4
ChaoticNeutralGnome <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2023-01-14 23:39:35 -0500

    Allow leader to put themself in game and have spies revealed to each other

--- Games/types/Resistance/roles/Spies/Spy.js
+++ Games/types/Resistance/roles/Spies/Spy.js
@@ -2,16 +2,16 @@ const Role = require("../../Role");
 
 module.exports = class Spy extends Role {
 
-	constructor(player, data) {
-		super("Spy", player, data);
+    constructor(player, data) {
+        super("Spy", player, data);
 
-		this.alignment = "Spies";
-		this.cards = ["TeamCore", "WinWithSpies"];
-		this.meetingMods = {
-			"Mission Success": {
-				flags: ["voting", "mustAct", "includeNo"]
-			}
-		};
-	}
+        this.alignment = "Spies";
+        this.cards = ["TeamCore", "WinWithSpies", "KnowSpies"];
+        this.meetingMods = {
+            "Mission Success": {
+                flags: ["voting", "mustAct", "includeNo"]
+            }
+        };
+    }
 
 }
