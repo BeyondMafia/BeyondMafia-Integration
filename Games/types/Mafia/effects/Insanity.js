b5ec993028ec9ea988e4bc591c581d38e299f635
R H <raycxhuang@gmail.com>
2023-01-16 09:19:32 +1100

    Update Insanity.js

--- Games/types/Mafia/effects/Insanity.js
+++ Games/types/Mafia/effects/Insanity.js
@@ -24,6 +24,24 @@ module.exports = class Insanity extends Effect {
 				message.content += chars[Random.randInt(0, chars.length - 1)];
 		}
 	}
+	
+	speakQuote(quote) {
+		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var originalLength = message.content.length;
+
+		message.content = "";
+		message.parseForReview = this.parseForReview;
+		message.modified = true;
+
+		for (let i = 0; i < originalLength; i++) {
+			let isSpace = Random.randFloat();
+
+			if (isSpace < 0.2)
+				message.content += " ";
+			else
+				message.content += chars[Random.randInt(0, chars.length - 1)];
+		}
+	}
 
 	parseForReview(message) {
 		message.content = message.versions["*"].content;
