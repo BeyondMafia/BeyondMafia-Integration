7203374b58e3bc351b05945adb43fd1bb8f719e9
ChaoticNeutralGnome <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2023-01-17 11:58:24 -0500

    Update Leader.js

--- Games/types/Resistance/items/Leader.js
+++ Games/types/Resistance/items/Leader.js
@@ -2,41 +2,41 @@ const Item = require("../Item");
 
 module.exports = class Leader extends Item {
 
-    constructor(game) {
-        super("Leader");
+	constructor(game) {
+		super("Leader");
 
-        this.listeners = {
-            "state": function(stateInfo) {
-                if (stateInfo.name.match(/Team Selection/)) {
-                    this.game.queueAlert(`${this.game.currentLeader.name} is the leader.`);
-                    this.game.queueAlert(`Team size: ${this.game.currentTeamSize}`);
-                }
-            }
-        };
-        this.meetings = {
-            "Assemble Team": {
-                states: ["Team Selection"],
-                flags: ["voting", "multi", "mustAct"],
-                targets: { include: ["alive"], exclude: [] },
-                multiMin: game.currentTeamSize,
-                multiMax: game.currentTeamSize,
-                action: {
-                    run: function() {
-                        for (let player of this.game.players) {
-                            player.role.meetings["Mission Success"].disabled = true;
-                            player.role.meetings["Approve Team"].disabled = false;
-                        }
+		this.listeners = {
+			"state": function(stateInfo) {
+				if (stateInfo.name.match(/Team Selection/)) {
+					this.game.queueAlert(`${this.game.currentLeader.name} is the leader.`);
+					this.game.queueAlert(`Team size: ${this.game.currentTeamSize}`);
+				}
+			}
+		};
+		this.meetings = {
+			"Assemble Team": {
+				states: ["Team Selection"],
+				flags: ["voting", "multi", "mustAct"],
+				targets: { include: ["alive"], exclude: [] },
+				multiMin: game.currentTeamSize,
+				multiMax: game.currentTeamSize,
+				action: {
+					run: function() {
+						for (let player of this.game.players) {
+							player.role.meetings["Mission Success"].disabled = true;
+							player.role.meetings["Approve Team"].disabled = false;
+						}
 
-                        for (let target of this.target)
-                            target.role.meetings["Mission Success"].disabled = false;
+						for (let target of this.target)
+							target.role.meetings["Mission Success"].disabled = false;
 
-                        this.actor.role.meetings["Approve Team"].disabled = true;
+						this.actor.role.meetings["Approve Team"].disabled = true;
 
-                        this.game.queueAlert(`Team selected: ${this.target.map(t => t.name).join(", ")}`);
-                        this.actor.dropItem("Leader");
-                    }
-                }
-            }
-        };
-    }
+						this.game.queueAlert(`Team selected: ${this.target.map(t => t.name).join(", ")}`);
+						this.actor.dropItem("Leader");
+					}
+				}
+			}
+		};
+	}
 };
