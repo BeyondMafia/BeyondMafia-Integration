2ef4ea00278a9b5b43708cb0a1c5226d23a139e3
r3ndd <3lliotn98@gmail.com>
2023-01-17 11:04:12 -0700

    Cancel quotes while insane

--- Games/types/Mafia/effects/Insanity.js
+++ Games/types/Mafia/effects/Insanity.js
@@ -3,49 +3,41 @@ const Random = require("../../../../lib/Random");
 
 module.exports = class Insanity extends Effect {
 
-	constructor() {
-		super("Insanity");
-	}
+    constructor() {
+        super("Insanity");
+    }
 
-	speak(message) {
-		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		var originalLength = message.content.length;
+    speak(message) {
+        message.content = this.makeStrInsane(message.content);
+        message.parseForReview = this.parseForReview;
+        message.modified = true;
+    }
 
-		message.content = "";
-		message.parseForReview = this.parseForReview;
-		message.modified = true;
+    speakQuote(quote) {
+        quote.cancel = true;
+    }
 
-		for (let i = 0; i < originalLength; i++) {
-			let isSpace = Random.randFloat();
+    parseForReview(message) {
+        message.content = message.versions["*"].content;
+        return message;
+    }
 
-			if (isSpace < 0.2)
-				message.content += " ";
-			else
-				message.content += chars[Random.randInt(0, chars.length - 1)];
-		}
-	}
-	
-	speakQuote(quote) {
-		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		var originalLength = quote.content.length;
+    makeStrInsane(str) {
+        var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var originalLength = str.length;
 
-		quote.content = "";
-		quote.parseForReview = this.parseForReview;
-		quote.modified = true;
+        str = "";
 
-		for (let i = 0; i < originalLength; i++) {
-			let isSpace = Random.randFloat();
+        for (let i = 0; i < originalLength; i++) {
+            let isSpace = Random.randFloat();
 
-			if (isSpace < 0.2)
-				quote.content += " ";
-			else
-				quote.content += chars[Random.randInt(0, chars.length - 1)];
-		}
-	}
+            if (isSpace < 0.2)
+                str += " ";
+            else
+                str += chars[Random.randInt(0, chars.length - 1)];
+        }
 
-	parseForReview(message) {
-		message.content = message.versions["*"].content;
-		return message;
-	}
+        return str;
+    }
 
 };
