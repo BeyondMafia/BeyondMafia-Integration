67db42164485afca04c688e0721afc81b197192b
r3ndd <3lliotn98@gmail.com>
2023-01-17 10:44:18 -0700

    Convert indent size

--- Games/types/Mafia/roles/cards/MeetWithCultists.js
+++ Games/types/Mafia/roles/cards/MeetWithCultists.js
@@ -3,25 +3,25 @@ const { PRIORITY_CULT_CONVERT } = require("../../const/Priority");
 
 module.exports = class MeetWithCultists extends Card {
 
-	constructor(role) {
-		super(role);
+    constructor(role) {
+        super(role);
 
-		this.meetings = {
-			"Cultists": {
-				actionName: "Convert",
-				states: ["Night"],
-				flags: ["group", "voting", "multiActor"],
-				targets: { include: ["alive"], exclude: ["Monsters"] },
-				action: {
-					labels: ["convert", "cultist"],
-					priority: PRIORITY_CULT_CONVERT,
-					run: function () {
-						if (this.dominates())
-							this.target.setRole("Cultist");
-					}
-				}
-			}
-		};
-	}
+        this.meetings = {
+            "Cultists": {
+                actionName: "Convert",
+                states: ["Night"],
+                flags: ["group", "voting", "multiActor"],
+                targets: { include: ["alive"], exclude: ["Monsters"] },
+                action: {
+                    labels: ["convert", "cultist"],
+                    priority: PRIORITY_CULT_CONVERT,
+                    run: function () {
+                        if (this.dominates())
+                            this.target.setRole("Cultist");
+                    }
+                }
+            }
+        };
+    }
 
 }
--- Games/types/Mafia/roles/cards/MeetWithMasons.js
+++ Games/types/Mafia/roles/cards/MeetWithMasons.js
@@ -3,25 +3,25 @@ const { PRIORITY_MASON_CONVERT } = require("../../const/Priority");
 
 module.exports = class MeetWithMasons extends Card {
 
-	constructor(role) {
-		super(role);
+    constructor(role) {
+        super(role);
 
-		this.meetings = {
-			"Masons": {
-				actionName: "Convert",
-				states: ["Night"],
-				flags: ["group", "speech", "voting", "multiActor"],
-				targets: { include: ["alive"], exclude: ["Mason"] },
-				action: {
-					labels: ["convert", "mason"],
-					priority: PRIORITY_MASON_CONVERT,
-					run: function () {
-						if (this.target.role.alignment != "Mafia" && this.dominates())
-							this.target.setRole("Mason");
-					}
-				}
-			}
-		};
-	}
+        this.meetings = {
+            "Masons": {
+                actionName: "Convert",
+                states: ["Night"],
+                flags: ["group", "speech", "voting", "multiActor"],
+                targets: { include: ["alive"], exclude: ["Mason"] },
+                action: {
+                    labels: ["convert", "mason"],
+                    priority: PRIORITY_MASON_CONVERT,
+                    run: function () {
+                        if (this.target.role.alignment != "Mafia" && this.dominates())
+                            this.target.setRole("Mason");
+                    }
+                }
+            }
+        };
+    }
 
 }
