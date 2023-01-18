84e8e29c8937fcfc9ceb508daa1bacedf2eb9551
r3ndd <3lliotn98@gmail.com>
2023-01-18 14:18:15 -0700

    Fixes

--- Games/types/Mafia/roles/cards/SnoopItems.js
+++ Games/types/Mafia/roles/cards/SnoopItems.js
@@ -12,27 +12,27 @@ module.exports = class SnoopItems extends Card {
                 flags: ["voting"],
                 action: {
                     priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
-                    run: function() {
+                    run: function () {
                         let items = this.target.items.map(a => a.name);
-                        let alert = `You snoop on ${this.target.name}during the night and find they are carrying`
+                        let alert = `You snoop on ${this.target.name} during the night and find they are carrying `;
+
                         if (items.length) {
                             let count = {};
-                            for (let item of items){
-                                if (item in count){
+
+                            for (let item of items) {
+                                if (item in count) {
                                     count[item] += 1;
                                 }
                                 else {
                                     count[item] = 1;
                                 }
                             }
-                            let result = ": ";
-                            Object.keys(count).map(key => {
-                                result += count[key] + " " + key + ", ";
-                            });
-                            alert += result.slice(0, -2) + '.';
+
+                            alert += Object.keys(count).map(key => "a " + key).join(", ") + ".";
                         } else {
-                            alert += ' nothing.';
+                            alert += "nothing.";
                         }
+
                         this.actor.queueAlert(alert);
                     }
                 }
