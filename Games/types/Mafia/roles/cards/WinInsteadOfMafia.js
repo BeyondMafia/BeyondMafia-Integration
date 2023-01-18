264a88502727ffb529f4809bc0b0b8330d1a7466
ChaoticNeutralGnome <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2023-01-18 10:49:15 -0500

    added-mastermind

---
+++ Games/types/Mafia/roles/cards/WinInsteadOfMafia.js
@@ -0,0 +1,22 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_NO_MAF } = require("../../const/Priority");
+
+module.exports = class WinWithMafia extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.winCheck = {
+            priority: PRIORITY_WIN_NO_MAF,
+            check: function (counts, winners, aliveCount) {
+                if (counts["Mafia"] >= aliveCount / 2 && aliveCount > 0)
+                    {
+                        winners.addPlayer(this.player, this.player.role.name);
+                        winners.removeGroup("Mafia");
+                    }
+
+            }
+        };
+    }
+
+}
