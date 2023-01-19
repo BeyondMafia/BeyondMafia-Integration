00538cbdd934b1a9806fddd5ec099770d8b4ac99
R H <raycxhuang@gmail.com>
2023-01-20 08:29:43 +1100

    Create CatLady.js

---
+++ Games/types/Mafia/roles/Mafia/CatLady.js
@@ -0,0 +1,11 @@
+const Role = require("../../Role");
+
+module.exports = class CatLady extends Role {
+
+	constructor(player, data) {
+		super("Cat Lady", player, data);
+		this.alignment = "Mafia";
+		this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "CatGiver"];
+	}
+
+}
