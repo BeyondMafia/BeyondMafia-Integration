1cd90bbf45fca305f46cae1b57e798ce17e12b73
R H <raycxhuang@gmail.com>
2023-01-20 16:31:40 +1100

    Update GunFramer.js

--- Games/types/Mafia/roles/cards/GunFramer.js
+++ Games/types/Mafia/roles/cards/GunFramer.js
@@ -16,8 +16,8 @@ module.exports = class GunFramer extends Card {
 				action: {
 					priority: PRIORITY_GUN_FRAME,
 					run: function () {
-						while (this.startItems.include("Gun") == true) {
-							this.startItems.splice(this.startItems.indexOf("Gun"), 1);
+						while (this.items.include("Gun") == true) {
+							this.items.splice(this.items.indexOf("Gun"), 1);
 							this.actor.holdItem("Gun", {reveal:true, shooter: this.target})
 						}
 					}
