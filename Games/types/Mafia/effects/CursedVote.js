663ced69d6c091de4a19e078a91b29def4592df8
ChaoticNeutralGnome <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2023-01-20 12:38:47 -0500

    added diabolist

---
+++ Games/types/Mafia/effects/CursedVote.js
@@ -0,0 +1,39 @@
+const Effect = require("../Effect");
+const Action = require("../Action");
+
+module.exports = class CursedVote extends Effect {
+
+    constructor(curser, target, lifespan) {
+        super("CursedVote");
+        this.curser = curser;
+        this.target = target;
+        this.lifespan = lifespan;
+
+        this.listeners = {
+            "vote": function (vote) {
+                if (vote.meeting.name === "Village" &&
+                    vote.voter === this.player &&
+                    vote.target === this.target.id){
+                    let action = new Action({
+                        actor: this.curser,
+                        target: this.player,
+                        game: this.game,
+                        labels: ["kill", "curse", "hidden"],
+                        effect: this,
+                        power: 2,
+                        run: function () {
+                            this.game.sendAlert(`${this.target.name} feels a cold chill run down their spine!`)
+                            if (this.dominates())
+                                this.target.kill("curse", this.actor, true);
+                            this.effect.remove();
+                        }
+                    });
+                    action.do();
+                }
+            }
+        };
+    }
+
+
+
+}
---
+++ Games/types/Mafia/roles/cards/CurseVote.js
@@ -0,0 +1,34 @@
+const Card = require("../../Card");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class CurseVote extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.meetings = {
+            "Victim": {
+                states: ["Night"],
+                flags: ["voting"],
+                action: {
+                    priority: PRIORITY_KILL_DEFAULT - 1,
+                    run: function () {
+                        this.actor.role.data.victim = this.target;
+                    }
+                }
+            },
+            "Target": {
+                states: ["Night"],
+                flags: ["voting"],
+                action: {
+                    priority: PRIORITY_KILL_DEFAULT,
+                    run: function () {
+                        this.actor.role.data.victim.giveEffect("CursedVote", this.actor, this.target, 1);
+                        delete this.actor.role.data.victim;
+                    }
+                }
+            }
+        };
+    }
+
+}
