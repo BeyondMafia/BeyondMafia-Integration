5e552b0b7271424914aa0de9659543d7b73993ae
r3ndd <3lliotn98@gmail.com>
2023-01-30 14:04:05 -0700

    React build

---
+++ react_main/build/precache-manifest.f3f350f022b0ff0596dbfeb6383aabc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d51d757b53949be7c719643f5d7cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27753136452596563fd1",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "34b63f433f7a425b9463",
+    "url": "/static/css/main.eb4608e2.chunk.css"
+  },
+  {
+    "revision": "27753136452596563fd1",
+    "url": "/static/js/2.bfa0ae91.chunk.js"
+  },
+  {
+    "revision": "6668b4f3e744c7001f8d714206ace1e6",
+    "url": "/static/js/2.bfa0ae91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b63f433f7a425b9463",
+    "url": "/static/js/main.f914818e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f914818e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
