596a8bd11ffb0cacbd630b30fce7df8f93d21c77
joyce <jycyeo@yahoo.com.sg>
2023-02-01 00:36:40 +0800

    tabs to spaces

--- Games/types/Mafia/roles/Mafia/CatLady.js
+++ Games/types/Mafia/roles/Mafia/CatLady.js
@@ -2,10 +2,10 @@ const Role = require("../../Role");
 
 module.exports = class CatLady extends Role {
 
-	constructor(player, data) {
-		super("Cat Lady", player, data);
-		this.alignment = "Mafia";
-		this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "CatGiver"];
-	}
+    constructor(player, data) {
+        super("Cat Lady", player, data);
+        this.alignment = "Mafia";
+        this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "CatGiver"];
+    }
 
 }
--- Games/types/Mafia/roles/cards/CatGiver.js
+++ Games/types/Mafia/roles/cards/CatGiver.js
@@ -2,22 +2,22 @@ const Card = require("../../Card");
 
 module.exports = class CatGiver extends Card {
 
-	constructor(role) {
-		super(role);
+    constructor(role) {
+        super(role);
 
-		this.meetings = {
-			"Give Cat": {
-				states: ["Day"],
-				flags: ["voting", "instant", "noVeg"],
-				action: {
-					labels: ["giveItem", "cat"],
-					run: function () {
-						this.target.holdItem("Cat", this.actor);
-						this.target.queueAlert("You have received a cat!");
-					}
-				}
-			}
-		};
-	}
+        this.meetings = {
+            "Give Cat": {
+                states: ["Day"],
+                flags: ["voting", "instant", "noVeg"],
+                action: {
+                    labels: ["giveItem", "cat"],
+                    run: function () {
+                        this.target.holdItem("Cat", this.actor);
+                        this.target.queueAlert("You have received a cat!");
+                    }
+                }
+            }
+        };
+    }
 
 }
