ef75494c7b0fc6a18444dd782147a8ea79599bd3
joyce <jycyeo@yahoo.com.sg>
2023-02-01 20:33:16 +0800

    wait for finish

--- Games/types/Mafia/roles/cards/WinIfTargetDead.js
+++ Games/types/Mafia/roles/cards/WinIfTargetDead.js
@@ -11,8 +11,13 @@ module.exports = class WinIfTargetDead extends Card {
         this.winCheck = {
             priority: PRIORITY_WIN_CHECK_DEFAULT,
             againOnFinished: true,
-            check: function (counts, winners, aliveCount) {
-                if (this.killer && this.killer.alive) {
+            check: function (counts, winners, aliveCount, confirmedFinished) {
+                if (!confirmedFinished && counts["Village"] != aliveCount) {
+                    // game not ended
+                    return;
+                }
+
+                if (this.killer && !this.killer.alive) {
                     winners.addPlayer(this.player, this.name);
                 }
             }
