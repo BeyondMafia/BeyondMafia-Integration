015e3d7bb64a143495c6ee942e13d3dd259888d7
DiSoRiEnTeDD <122717543+DiSoRiEnTeDD@users.noreply.github.com>
2023-02-04 11:46:25 -0500

    Update ReceiveReports.js

--- Games/types/Mafia/roles/cards/ReceiveReports.js
+++ Games/types/Mafia/roles/cards/ReceiveReports.js
@@ -1,40 +1,37 @@
 const Card = require("../../Card");
-const { PRIORITY_RECIEVE_REPORTS } = require("../../const/Priority")
+const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
 
-module.exports = class RecieveReports extends card {
+module.exports = class AlertLearner extends Card {
 
     constructor(role) {
         super(role);
 
         this.meetings = {
             "Receive Reports": {
-                states: ["night"],
+                states: ["Night"],
                 flags: ["voting"],
+                targets: { include: ["alive"], exclude: ["self"] },
                 action: {
-                    priority: PRIORITY_RECIEVE_REPORTS,
+                    labels: ["investigate", "alerts"],
+                    priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT + 1,
                     run: function () {
-                        var allReports = [];
-                        var allAlerts = this.game.states[this.game.currentState];
+                        var reports = []
 
-                        for (let alert in allAlerts) {
-                            if (alert.recipients.includes(this.target)) {
-                                allReports.push(alert.message)
+                        for (let alert of this.game.alertQueue) {
+                            for (let recipient of alert.recipients) {
+                                if (recipient == this.target) {
+                                    reports.push(alert.message);
+                                }
                             }
                         }
 
-                        if (allReports.length) == 0 {
-                            this.game.QueueAlert("no reports")
-                        } else {
-                            var report = allReports.join('\n');
-                            this.game.queueAlert(report...);
-                        }
-                        
-                            }
-                        }
-
-                       
+                        var alert = `You received all reports that ${this.target.name} received: (${reports.join(',\n')}).`;
+                        this.actor.queueAlert(alert);
                     }
                 }
             }
+        };
+    }
 
+}
  
