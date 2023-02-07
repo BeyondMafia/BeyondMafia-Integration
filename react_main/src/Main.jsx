d11866fb6def33638a909bc645f5f8fff751500e
DrSharky <keleblanc24@gmail.com>
2023-02-07 18:27:23 -0500

    Fixed logged out null ref error
    Had to update this because it would have a null ref if the user is logged out and won't load the page.

--- react_main/src/Main.jsx
+++ react_main/src/Main.jsx
@@ -44,17 +44,19 @@ function Main() {
             .catch(errorAlert);
     }
 
-    if(user.settings.siteColorScheme){
-        if(document.documentElement.classList.contains("light-mode")){
-            document.documentElement.classList.remove("light-mode");
-            }
-        document.documentElement.classList.add("dark-mode");
-    }
-    else{
-        if(document.documentElement.classList.contains("dark-mode")) {
-            document.documentElement.classList.remove("dark-mode");
+    if(user.settings != null){
+        if(user.settings.siteColorScheme){
+            if(document.documentElement.classList.contains("light-mode")){
+                document.documentElement.classList.remove("light-mode");
+                }
+            document.documentElement.classList.add("dark-mode");
         }
-        document.documentElement.classList.add("light-mode");
+        else{
+            if(document.documentElement.classList.contains("dark-mode")) {
+                document.documentElement.classList.remove("dark-mode");
+            }
+            document.documentElement.classList.add("light-mode");
+        }
     }
 
     useEffect(() => {
