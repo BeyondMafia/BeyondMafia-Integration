4cc6d1d2642875f7ca97171ff0a244f4356b7c3a
starrydash <icypyromancer@gmail.com>
2023-02-12 16:06:06 -0800

    Mortician Changes
    Uses meeting mods to use same card as tracker but targeting dead instead.

--- Games/types/Mafia/roles/Village/Mortician.js
+++ Games/types/Mafia/roles/Village/Mortician.js
@@ -5,7 +5,12 @@ module.exports = class Mortician extends Role {
     constructor(player, data) {
         super("Mortician", player, data);
         this.alignment = "Village";
-        this.cards = ["VillageCore", "WinWithVillage", "RoleLearnerDead"];
+        this.cards = ["VillageCore", "WinWithVillage", "RoleLearner"];
+        this.meetingMods = {
+            "Block": {
+                targets: { include: ["dead"], exclude: ["alive", "self"] },
+            }
+        };
     }
 
 }
