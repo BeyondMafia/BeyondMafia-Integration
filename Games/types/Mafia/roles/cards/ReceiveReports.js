7cd92d658c337f9fd31ae5f91b6a2e7446b8b441
ChaoticNeutralGnome <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2023-02-21 10:41:33 -0500

    added no report message

--- Games/types/Mafia/roles/cards/ReceiveReports.js
+++ Games/types/Mafia/roles/cards/ReceiveReports.js
@@ -15,21 +15,23 @@ module.exports = class ReceiveReports extends Card {
                     labels: ["investigate", "alerts"],
                     priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT + 1,
                     run: function () {
-                        var reports = []
+                        let reports = []
 
                         for (let alert of this.game.alertQueue) {
                             if (!alert.recipients) {
                                 continue
                             }
-                            
                             for (let recipient of alert.recipients) {
                                 if (recipient == this.target) {
                                     reports.push(alert.message);
                                 }
                             }
                         }
-
-                        var alert = `You received all reports that ${this.target.name} received: (${reports.join(',\n')}).`;
+                        let alert;
+                        if (reports.length)
+                            alert = `You received all reports that ${this.target.name} received: (${reports.join(',\n')}).`;
+                        else
+                            alert = `${this.target.name} received no reports.`
                         this.actor.queueAlert(alert);
                     }
                 }
