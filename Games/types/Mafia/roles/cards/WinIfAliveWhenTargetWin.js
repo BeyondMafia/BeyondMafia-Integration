d09ba8a044c2b994b861fe0bbf2558ce83769383
R H <raycxhuang@gmail.com>
2023-02-23 17:34:17 +1100

    Update WinIfAliveWhenTargetWin.js

--- Games/types/Mafia/roles/cards/WinIfAliveWhenTargetWin.js
+++ Games/types/Mafia/roles/cards/WinIfAliveWhenTargetWin.js
@@ -1,16 +1,30 @@
 const Card = require("../../Card");
 const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
 
 module.exports = class WinIfAliveWhenTargetWin extends Card {
 
     constructor(role) {
         super(role);
         
+        this.meetings = {
+            "Gain Alignment": {
+                states: ["Night"],
+                flags: ["voting"],
+                action: {
+                    labels: ["block"],
+                    priority: PRIORITY_GAIN_ALIGNMENT,
+                    run: function () {
+                        this.data.targetName = this.target.name;
+                    }
+                }
+            }
+        
         this.winCheck = {
             priority: PRIORITY_WIN_CHECK_DEFAULT,
             againOnFinished: true,
             check: function (counts, winners, aliveCount, confirmedFinished) {
-                if (this.player.alive && confirmedFinished && !winners.groups[this.data.targetAlignment]) {
+                if (this.player.alive && confirmedFinished && winners.groups[this.data.targetName]) {
                     winners.addPlayer(this.player, this.name);
                 }
             }
