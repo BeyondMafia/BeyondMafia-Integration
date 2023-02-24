839d0a5fdb275b3a0989f1479ea554be6508bf4e
R H <raycxhuang@gmail.com>
2023-02-24 21:43:08 +1100

    Create Autocrat.js

---
+++ Games/types/Mafia/roles/Independent/Autocrat.js
@@ -0,0 +1,13 @@
+const Role = require("../../Role");
+
+module.exports = class Autocrat extends Role {
+
+    constructor(player, data) {
+        super("Autocrat", player, data);
+
+        this.alignment = "Independent";
+        this.winCount = "Village";
+        this.cards = ["VillageCore", "MeetingMafia", "WinInsteadOfVillage"];
+    }
+
+}
