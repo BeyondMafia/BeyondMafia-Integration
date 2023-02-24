35a6a13d1ae8f90ebfdb70d8f108d63822d18911
R H <raycxhuang@gmail.com>
2023-02-25 09:02:14 +1100

    Create KnowMafia.js

---
+++ Games/types/Mafia/roles/cards/KnowMafia.js
@@ -0,0 +1,24 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class KnowMafia extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.actions = [
+            {
+                labels: ["knowledge"],
+                priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+                run: function () {
+                  let mafia = this.game.players.filter(p => p.role.alignment == "Mafia");
+                  var alert = `You know that ${mafia.join(',\n')} are the Mafia.`;
+                  if (this.game.getStateInfo().dayCount == 0) {
+                    this.actor.queueAlert(alert);
+                  }
+                }
+            }
+        ];
+    }
+}
