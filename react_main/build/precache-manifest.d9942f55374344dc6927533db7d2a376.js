5d4cde17b6e390b82165b267178c3e7869c6b783
Andrei <andreiii500@yahoo.com>
2023-02-24 03:10:03 +0200

    chore: build react (correctly)

---
+++ react_main/build/precache-manifest.d9942f55374344dc6927533db7d2a376.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f875099ca348b3e48a6148f62cc681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5955b2d5ea7168477ff",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "ce72a32f5ec0ffb9ddd8",
+    "url": "/static/css/main.ba159366.chunk.css"
+  },
+  {
+    "revision": "c5955b2d5ea7168477ff",
+    "url": "/static/js/2.a88047f4.chunk.js"
+  },
+  {
+    "revision": "8aa342599c46df678da4165589673928",
+    "url": "/static/js/2.a88047f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce72a32f5ec0ffb9ddd8",
+    "url": "/static/js/main.5e5e14d7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5e5e14d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
