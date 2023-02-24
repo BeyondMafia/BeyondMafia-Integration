7f47a0db99a835b9b168c62065f19cc4012001c7
R H <raycxhuang@gmail.com>
2023-02-25 09:03:24 +1100

    Create Merlin.js

---
+++ Games/types/Mafia/roles/Village/Merlin.js
@@ -0,0 +1,11 @@
+const Role = require("../../Role");
+
+module.exports = class Merlin extends Role {
+
+    constructor(player, data) {
+        super("Villager", player, data);
+        this.alignment = "Village";
+        this.cards = ["VillageCore", "WinWithVillage", "KnowMafia"];
+    }
+
+}
