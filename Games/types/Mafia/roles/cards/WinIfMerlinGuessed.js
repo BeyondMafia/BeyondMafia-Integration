6010d29ae2a7e1f43c2aad563e082977dc9ef9a7
R H <raycxhuang@gmail.com>
2023-02-25 10:55:14 +1100

    Create WinIfMerlinGuessed.js

---
+++ Games/types/Mafia/roles/cards/WinIfMerlinGuessed.js
@@ -0,0 +1,41 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_GUESS_MERLIN } = require("../../const/Priority");
+
+module.exports = class WinIfMerlinGuessed extends Card {
+
+    constructor(role) {
+        super(role);
+      
+      this.meetings = {
+            "Guess Merlin": {
+                states: ["Night"],
+                flags: ["voting"],
+                shouldMeet: function () {
+                    for (let player of this.game.players)
+                        if (player.hasItem("Guess"))
+                            return true;
+
+                    return false;
+                },
+                action: {
+                    labels: ["visit"],
+                    priority: PRIORITY_GUESS_MERLIN,
+                    run: function () {
+                        if (this.target.role = "Merlin") {
+                            this.data.guessedMerlin = "true";
+                        }
+                    }
+                }
+            }
+
+        this.winCheck = {
+            priority: PRIORITY_WIN_CHECK_DEFAULT,
+            check: function (counts, winners, aliveCount) {
+                if (this.data.guessedMerlin == "true")
+                    winners.addPlayer(this.player, this.player.role.alignment == "Mafia" ? "Mafia" : this.player.role.name);
+            }
+        };
+    }
+
+}
