a336239b83d1f0764f09e918da01f249393540e2
R H <raycxhuang@gmail.com>
2023-02-25 10:41:43 +1100

    Update Guess.js

--- Games/types/Mafia/items/Guess.js
+++ Games/types/Mafia/items/Guess.js
@@ -2,24 +2,9 @@ const Item = require("../Item");
 
 module.exports = class Guess extends Item {
 
-    constructor(reveal) {
+    constructor() {
         super("Guess");
-
-        this.reveal = reveal;
-        this.lifespan = 1;
-        this.cannotBeStolen = true;
-
-        this.meetings = {
-            "Guess Merlin": {
-                actionName: "Guess",
-                states: ["Night"],
-                flags: ["group", "voting"],
-                run: function () {
-                  if this.target.role == "Merlin" {
-                    return this.holder.data.guessedMerlin = true;
-                  }
-                }
-            }
-        };
     }
+
+
 }
