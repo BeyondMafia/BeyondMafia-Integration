ca4402d17f8e4aa72e54a6d148a4c2fdd76c474c
Andrei <andreiii500@yahoo.com>
2023-02-24 14:10:38 +0200

    fix: only allow KLing in village meetings

--- Games/core/Meeting.js
+++ Games/core/Meeting.js
@@ -638,7 +638,7 @@ module.exports = class Meeting {
                     (!this.multi && this.votes[member.id] == null) ||
                     (this.multi && selections.length < this.multiMin && selections.indexOf("*") == -1)
                 ) {
-                    const playerIsKicklynched = this.finalTarget === member.id;
+                    const playerIsKicklynched = this.actionName === "Village Vote" && this.finalTarget === member.id;
                     this.game.vegPlayer(member.player, playerIsKicklynched);
                 }
             }
