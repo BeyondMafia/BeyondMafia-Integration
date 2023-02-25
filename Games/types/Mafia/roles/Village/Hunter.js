337c1369dc66092b4f91202a7d8b039edb764fb7
R H <raycxhuang@gmail.com>
2023-02-25 11:50:34 +1100

    Update and rename Archer.js to Hunter.js

--- Games/types/Mafia/roles/Village/Hunter.js
+++ Games/types/Mafia/roles/Village/Hunter.js
@@ -1,9 +1,9 @@
 const Role = require("../../Role");
 
-module.exports = class Archer extends Role {
+module.exports = class Hunter extends Role {
 
     constructor(player, data) {
-        super("Archer", player, data);
+        super("Hunter", player, data);
 
         this.alignment = "Village";
         this.cards = ["VillageCore", "WinWithVillage", "LynchRevenge"];
