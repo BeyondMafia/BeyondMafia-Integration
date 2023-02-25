a1b71ed9974a2ab46a112d07a5f787e4ea7e8651
R H <raycxhuang@gmail.com>
2023-02-25 15:17:59 +1100

    Create Gambler.js
    Signed-off-by: R H <raycxhuang@gmail.com>

---
+++ Games/types/Mafia/roles/Independent/Gambler.js
@@ -0,0 +1,13 @@
+const Role = require("../../Role");
+
+module.exports = class Gambler extends Role {
+
+    constructor(player, data) {
+        super("Gambler", player, data);
+
+        this.alignment = "Independent";
+        this.winCount = "Village";
+        this.cards = ["VillageCore", "Gamble", "WinIfBeatThrice"];
+    }
+
+}
