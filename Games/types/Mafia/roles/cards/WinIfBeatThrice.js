b01f982d65ef0b52cfb508c1f3ef5b3d518e1772
R H <raycxhuang@gmail.com>
2023-02-25 15:35:33 +1100

    Create WinIfBeatThrice.js
    Signed-off-by: R H <raycxhuang@gmail.com>

---
+++ Games/types/Mafia/roles/cards/WinIfBeatThrice.js
@@ -0,0 +1,20 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+
+module.exports = class WinIfBeatThrice extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.winCheck = {
+            priority: PRIORITY_WIN_CHECK_DEFAULT,
+            againOnFinished: true,
+            check: function (counts, winners, aliveCount, confirmedFinished) {
+                if (this.player.data.challengeWon == 3) {
+                    winners.addPlayer(this.player, this.name);
+                }
+            }
+        };
+    }
+
+}
