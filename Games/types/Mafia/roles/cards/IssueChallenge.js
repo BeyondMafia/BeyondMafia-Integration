6e3a377e27ca6507b67d24466b5411934c82ecdd
R H <raycxhuang@gmail.com>
2023-02-25 17:40:51 +1100

    Update IssueChallenge.js
    Signed-off-by: R H <raycxhuang@gmail.com>

--- Games/types/Mafia/roles/cards/IssueChallenge.js
+++ Games/types/Mafia/roles/cards/IssueChallenge.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_DAY_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_CHALLENGE, PRIORITY_DAY_DEFAULT } = require("../../const/Priority");
 
 module.exports = class IssueChallenge extends Card {
 
@@ -7,52 +7,71 @@ module.exports = class IssueChallenge extends Card {
         super(role);
 
         this.meetings = {
-            "Issue Challenge": {
+            "Jail Target": {
                 states: ["Day"],
                 flags: ["voting"],
                 action: {
-                    labels: ["jail"],
+                    labels: ["challenge"],
                     priority: PRIORITY_DAY_DEFAULT,
                     run: function () {
                         if (this.dominates()) {
                             this.target.holdItem("Challenge");
-                            this.actor.role.data.challenged = this.target;
-                        }
+                            this.actor.holdItem("Challenge");
+                            this.data.chalenged = this.target;
+                            this.data.chalenger = this.actor;}
                     }
                 }
             },
-            "Challenger": {
-                actionName: "Challenge",
+            "RPS A": {
+                actionName: "Rock, Paper, Scissors",
                 states: ["Night"],
-                flags: ["voting"],
+                flags: ["exclusive", "voting"],
                 inputType: "alignment",
-                targets: ["Rock", "Paper", "Scissors"],
-                action: {
-                    labels: ["challenge"],
-                    priority: PRIORITY_CHALLENGE,
-                    run: function () {
-                        this.data.rps1 = this.target;
-                    }
-                },
-            "Challenged": {
-                actionName: "Challenge",
-                states: ["Night"],
-                flags: ["voting"],
-                inputType: "alignment",
-                targets: ["Rock", "Paper", "Scissors"],
+                targets: ['Rock', 'Paper', 'Scissors'],
                 shouldMeet: function () {
-                    if (this.actor.hasItem("Challenge"))
-                        return true;
-                    
+                    for (let player of this.game.players)
+                        if (player.hasItem("Challenge"))
+                            return true;
+
                     return false;
                 },
                 action: {
                     labels: ["challenge"],
                     priority: PRIORITY_CHALLENGE,
                     run: function () {
-                        this.data.rps2 = this.target;
+                        var challenged = this.data.chalenged;
+
+                        if (challenged)
+                            return;
+                        this.data.rpsA = this.target;
                     }
+                }
+            },
+            "RPS B": {
+                actionName: "Rock, Paper, Scissors",
+                states: ["Night"],
+                flags: ["exclusive", "voting"],
+                inputType: "alignment",
+                targets: ['Rock', 'Paper', 'Scissors'],
+                shouldMeet: function () {
+                    for (let player of this.game.players)
+                        if (player.hasItem("Challenge"))
+                            return true;
+
+                    return false;
                 },
+                action: {
+                    labels: ["challenge"],
+                    priority: PRIORITY_CHALLENGE,
+                    run: function () {
+                        var challenger = this.data.chalenger;
+
+                        if (challenger)
+                            return;
+                        this.data.rpsB = this.target;
+                    }
+                }
+            }
         };
     }
 
