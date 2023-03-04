870fe21be2011a36845bdd41ce3939eb72b86c5a
DrSharky <keleblanc24@gmail.com>
2023-03-03 19:19:08 -0500

    Fixed dark mode typing bubbles

--- react_main/src/pages/Game/Game.jsx
+++ react_main/src/pages/Game/Game.jsx
@@ -1486,7 +1486,7 @@ export function PlayerRows(props) {
                 <ReactLoading
                     className={`typing-icon ${props.stateViewing != -1 ? "has-role" : ""}`}
                     type="bubbles"
-                    color="black"
+                    color={ document.documentElement.classList[0].includes("dark") ?  "white" : "black"}
                     width="20"
                     height="20" />
             }
