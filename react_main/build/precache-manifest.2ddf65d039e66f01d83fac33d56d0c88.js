c4ff35278205a9a7a70de3375f089d1d0eea8357
DrSharky <keleblanc24@gmail.com>
2023-03-03 20:47:15 -0500

    react build

---
+++ react_main/build/precache-manifest.2ddf65d039e66f01d83fac33d56d0c88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1dfd50756a4fd52e2ed0f313eb949c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece89c769686c9333726",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "34897339947d61804b99",
+    "url": "/static/css/main.66b3367b.chunk.css"
+  },
+  {
+    "revision": "ece89c769686c9333726",
+    "url": "/static/js/2.dfcee42f.chunk.js"
+  },
+  {
+    "revision": "8aa342599c46df678da4165589673928",
+    "url": "/static/js/2.dfcee42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34897339947d61804b99",
+    "url": "/static/js/main.a6899503.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a6899503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
--- react_main/build/service-worker.js
+++ react_main/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bffa1d265c4ec3803cd07466ea9d6b4d.js"
+  "/precache-manifest.2ddf65d039e66f01d83fac33d56d0c88.js"
 );
 
 self.addEventListener('message', (event) => {
