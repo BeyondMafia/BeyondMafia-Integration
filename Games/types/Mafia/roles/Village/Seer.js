24ccc9e00fc0640e4a84d853d111e20a42ead68f
R H <104711111+R9H9@users.noreply.github.com>
2023-03-05 19:16:04 +1100

    Update Seer.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- Games/types/Mafia/roles/Village/Seer.js
+++ Games/types/Mafia/roles/Village/Seer.js
@@ -3,7 +3,7 @@ const Role = require("../../Role");
 module.exports = class Seer extends Role {
 
     constructor(player, data) {
-        super("Villager", player, data);
+        super("Seer", player, data);
         this.alignment = "Village";
         this.cards = ["VillageCore", "WinWithVillage", "KnowMafiaSecret"];
     }
