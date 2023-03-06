c97abf5c74b9776012b12f64537a488b5cf589de
R H <104711111+R9H9@users.noreply.github.com>
2023-03-06 17:48:16 +1100

    Update AbsentMinded.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- Games/types/Mafia/roles/cards/AbsentMinded.js
+++ Games/types/Mafia/roles/cards/AbsentMinded.js
@@ -6,16 +6,21 @@ module.exports = class AbsentMinded extends Card {
       super(role);
       
       if (this.player.role.alignment == "Village") {
-        this.player.data.appearAs = "Visitor";
+          this.player.data.character = "Visitor";
+          this.player.data.plain = true;
       } else if (this.player.role.alignment == "Mafia") {
-        this.player.data.appearAs = "Trespasser";
+          this.player.data.character = "Trespasser";
+          this.player.data.plain = true;
+      } else {
+          this.player.data.character = "real";
+          this.player.data.plain = false;
       }
       
       this.appearance = {
-        self: this.player.data.appearAs
+          self: this.player.data.character
       };
       this.hideModifier = {
-        self: true
+          self: this.player.data.plain
       };
     }
 
