6127c4001fd58c84373ab479a9110288b80a6672
R H <104711111+R9H9@users.noreply.github.com>
2023-03-07 20:59:57 +1100

    Update Settings.jsx
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- react_main/src/pages/User/Settings.jsx
+++ react_main/src/pages/User/Settings.jsx
@@ -55,7 +55,17 @@ export default function Settings(props) {
 		{
 			label: "Site Color Scheme",
 			ref: "siteColorScheme",
-			type: "boolean",
+			type: "select",
+			options: [
+				{
+					label: "Light",
+					value: "light"
+				},
+				{
+					label: "Dark",
+					value: "dark"
+				}
+			],
 		}
 	]);
 
