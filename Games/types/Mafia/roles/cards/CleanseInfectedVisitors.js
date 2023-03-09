2802fc0a8322b86bcb1164803fee3a5ec1075255
R H <104711111+R9H9@users.noreply.github.com>
2023-03-09 18:53:28 +1100

    Update and rename CleanseWerewolfVisitors.js to CleanseInfectedVisitors.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- Games/types/Mafia/roles/cards/CleanseInfectedVisitors.js
+++ Games/types/Mafia/roles/cards/CleanseInfectedVisitors.js
@@ -1,41 +1,41 @@
 const Card = require("../../Card");
-const { PRIORITY_CLEANSE_WEREWOLF_VISITORS } = require("../../const/Priority");
+const { PRIORITY_CLEANSE_INFECTED_VISITORS } = require("../../const/Priority");
 
-module.exports = class CleanseWerewolfVisitors extends Card {
+module.exports = class CleanseInfectedVisitors extends Card {
 
     constructor(role) {
         super(role);
 
         this.actions = [
             {
-                priority: PRIORITY_CLEANSE_WEREWOLF_VISITORS,
-                labels: ["cleanse", "werewolf", "hidden"],
+                priority: PRIORITY_CLEANSE_INFECTED_VISITORS,
+                labels: ["cleanse", "infected", "hidden"],
                 run: function () {
                     if (this.game.getStateName() != "Night")
                         return;
 
-                    var cleansedWolves = {};
+                    var cleansedInfected = {};
 
                     for (let action of this.game.actions[0]) {
                         if (
                             action.target == this.actor &&
-                            action.actor.hasEffect("Werewolf") &&
+                            action.actor.hasEffect("Infected") &&
                             action.priority > this.priority &&
                             !action.hasLabel("hidden")
                         ) {
-                            action.actor.removeEffect("Werewolf", true);
-                            cleansedWolves[action.actor.id] = true;
+                            action.actor.removeEffect("Infected", true);
+                            cleansedInfected[action.actor.id] = true;
                         }
                     }
 
-                    if (Object.keys(cleansedWolves).length == 0)
+                    if (Object.keys(cleansedInfected).length == 0)
                         return;
 
                     for (let action of this.game.actions[0]) {
                         if (
                             action.actor &&
-                            cleansedWolves[action.actor.id] &&
-                            action.hasLabels(["kill", "werewolf"])
+                            cleansedInfected[action.actor.id] &&
+                            action.hasLabels(["kill", "infected"])
                         ) {
                             action.cancel();
                         }
