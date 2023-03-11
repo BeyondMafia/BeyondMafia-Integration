6af1d56a54e803f2e61f78a060641dc03c3836cb
R H <104711111+R9H9@users.noreply.github.com>
2023-03-11 17:56:49 +1100

    Update LearnMafia.jsx
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- react_main/src/pages/Play/Learn/LearnMafia.jsx
+++ react_main/src/pages/Play/Learn/LearnMafia.jsx
@@ -96,7 +96,7 @@ export default function LearnMafia(props) {
 		},
 		{
 			name: "Absent-Minded",
-			text: "Appears as Visitor (if Village-aligned) or Trespasser (if Mafia-aligned) to self with no modifier.",
+			text: "Appears as Visitor (if aligned with Village) or Trespasser (if aligned with Mafia) to self with no modifier.",
 			icon: <div className="icon modifier modifier-Mafia-Absent-Minded" />
 		},
 		{
