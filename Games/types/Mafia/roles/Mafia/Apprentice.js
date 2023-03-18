6f2739b32ba98f66326173c35b527d9c98aee19c
R H <104711111+R9H9@users.noreply.github.com>
2023-03-18 11:57:37 +1100

    Create Apprentice.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

---
+++ Games/types/Mafia/roles/Mafia/Apprentice.js
@@ -0,0 +1,21 @@
+const Role = require("../../Role");
+
+module.exports = class Apprentice extends Role {
+
+    constructor(player, data) {
+        super("Apprentice", player, data);
+
+        this.alignment = "Mafia";
+        this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "BecomeDeadRole"];
+        this.meetingMods = {
+            "Become Role": {
+                targets: { include: ["Mafia"], exclude: ["alive", "self", targetFilter] },
+            }
+        };
+    }
+
+}
+
+function targetFilter(player) {
+    return !this.player.deathType == "lynch";
+}
