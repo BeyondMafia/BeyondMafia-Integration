dce0bd9cd3c67f1b5d10b4482bfa30c9d825fcee
pikulet <24848927+pikulet@users.noreply.github.com>
2023-03-22 12:26:24 +0800

    feat: add alert for slasher and tink getting items (#533)

--- Games/types/Mafia/roles/cards/DayShooter.js
+++ Games/types/Mafia/roles/cards/DayShooter.js
@@ -18,6 +18,7 @@ module.exports = class DayShooter extends Card {
 
                     if(!visitors?.length){
                         this.actor.holdItem("Gun");
+                        this.queueGetItemAlert("Gun", this.actor);
                     }
                 }
             }
