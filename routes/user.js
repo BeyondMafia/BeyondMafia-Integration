8fea811fee6fcbe69a912479318d5f1c11cbf6bc
R H <104711111+R9H9@users.noreply.github.com>
2023-03-27 16:40:46 +1100

    Update user.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- routes/user.js
+++ routes/user.js
@@ -454,7 +454,7 @@ router.post("/settings/update", async function (req, res) {
             const yiq = (rgb[0] * 2126 + rgb[1] * 7152 + rgb[2] * 722) / 10000;
             const isLight = yiq >= contrastTolerance * 256;
 
-            if (isLight) {
+            if ((prop == "backgroundColor" || prop == "textColor" || prop == "nameColor") && isLight) {
                 res.status(500);
                 res.send("Color is too light.");
                 return;
