77228e9a8f98d36a61c0ade075d4326ee7528e74
R H <104711111+R9H9@users.noreply.github.com>
2023-03-28 19:15:16 +1100

    Update Game.jsx
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

--- react_main/src/pages/Game/Game.jsx
+++ react_main/src/pages/Game/Game.jsx
@@ -1536,7 +1536,7 @@ export function PlayerRows(props) {
                     <ReactLoading
                         className={`typing-icon ${props.stateViewing != -1 ? "has-role" : ""}`}
                         type="bubbles"
-                        color={ document.documentElement.classList[0].includes("dark") ?  "white" : "black"}
+                        color={ (document.documentElement.classList.includes("dark") || window.matchMedia("(prefers-color-scheme: dark)");) ?  "white" : "black"}
                         width="20"
                         height="20" />
                 }
