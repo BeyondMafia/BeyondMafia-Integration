abc672a078573f8ac54f211fd0cfb9dc6bc2c3d1
milkshakesatdawn <78222483+milkshakesatdawn@users.noreply.github.com>
2023-03-30 16:09:29 +0000

    framer take 2

---
+++ Games/types/Mafia/roles/cards/MakeGuilty.js
@@ -0,0 +1,24 @@
+const Card = require("../../Card");
+const { PRIORITY_MAKE_GUILTY } = require("../../const/Priority");
+
+module.exports = class MakeInnocent extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.meetings = {
+            "Make Guilty": {
+                states: ["Night"],
+                flags: ["voting"],
+                targets: { include: ["alive"], exclude: ["self"] },
+                action: {
+                    priority: PRIORITY_MAKE_GUILTY,
+                    run: function () {
+                        this.target.setTempAppearance("investigate", "Mafia");
+                    }
+                }
+            }
+        };
+    }
+
+}
