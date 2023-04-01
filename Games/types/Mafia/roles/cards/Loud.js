4fc72755283be36366b9a80be600e3afe2adb841
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-01 15:20:54 +0800

    combine loud and journ into action library

--- Games/types/Mafia/roles/cards/Loud.js
+++ Games/types/Mafia/roles/cards/Loud.js
@@ -1,32 +1,25 @@
 const Card = require("../../Card");
+const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
 
 module.exports = class Loud extends Card {
 
     constructor(role) {
         super(role);
 
-        this.listeners = {
-            "state": function (){
-                if (this.game.setup.startState == "Night" && this.game.getStateInfo().name == "Night 0"){
-                    return;
-                }
+        this.actions = [
+            {
+                priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT + 2,
+                labels: ["investigate", "alerts"],
+                run: function () {
+                    if (this.game.getStateName() != "Night")
+                        return;
 
-                if (this.game.setup.startState == "Day" && this.game.getStateInfo().name == "Day 1"){
-                    return;
-                }
-
-                for (let alert of this.game.alertQueue) {
-                    if (!alert.recipients) {
-                        continue
-                    }
-                    for (let recipient of alert.recipients) {
-                        if (recipient == this.player &&
-                            !alert.message.startsWith("Graveyard participation")) {
-                            this.game.queueAlert(`A Loud ${this.player.role.name} is overheard reading: ${alert.message}`);
-                        }
+                    let reports = this.getReports(this.actor);
+                    for (let report of reports) {
+                        this.game.queueAlert(`A Loud ${this.actor.role.name} is overheard reading: ${report}`);
                     }
                 }
             }
-        }
+        ];
     }
 }
