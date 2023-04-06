52356c04ff410f812e8192715ee1eed2b03de55c
R H <104711111+R9H9@users.noreply.github.com>
2023-04-06 11:55:00 +1000

    Create WinInsteadOfVillage.js
    Signed-off-by: R H <104711111+R9H9@users.noreply.github.com>

---
+++ Games/types/Mafia/roles/cards/WinInsteadOfVillage.js
@@ -0,0 +1,20 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+
+module.exports = class WinWithVillage extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.winCheck = {
+            priority: PRIORITY_WIN_CHECK_DEFAULT,
+            check: function (counts, winners, aliveCount) {
+                if (counts["Village"] == aliveCount && aliveCount > 0) {
+                  winners.addPlayer(this.player, this.player.role.name);
+                  winners.removeGroup("Village");
+                }
+            }
+        };
+    }
+
+}
