f9544886ae0f3f7bae51090f74cb2d7dabdbe996
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-06 21:08:23 +0530

    fix: string passed to queueAlert function missing 'sy2c'

--- Games/types/Mafia/roles/cards/BreadGiver.js
+++ Games/types/Mafia/roles/cards/BreadGiver.js
@@ -27,7 +27,7 @@ module.exports = class BreadGiver extends Card {
                     priority: PRIORITY_ITEM_GIVER_DEFAULT,
                     run: function () {
                         this.target.holdItem("Bread");
-                        this.target.queueAlert("You have received a bread!");
+                        this.target.queueAlert(":sy2c: You have received a bread!");
                     }
                 }
             }
