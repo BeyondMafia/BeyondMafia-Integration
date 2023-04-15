7df93bbff9d69b3a0ae6fa579d9ecca148f07841
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-15 23:06:49 +0800

    feat: add ventriloquist role

---
+++ Games/types/Mafia/roles/cards/ControlPuppet.js
@@ -0,0 +1,19 @@
+const Card = require("../../Card");
+
+module.exports = class ControlPuppet extends Card {
+
+    constructor(role) {
+        super(role);
+    }
+
+    speak(message) {
+        if (message.abilityName != "Control Puppet")
+            return;
+
+        message.modified = true;
+        
+        let puppet = this.role.game.getPlayer(message.abilityTarget);
+        message.sender = puppet;
+    }
+
+}
