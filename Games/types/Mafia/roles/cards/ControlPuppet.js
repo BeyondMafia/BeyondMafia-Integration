a6df1aa05d0e98cbf5701c7e04b2b9ec6f1a154e
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-16 00:01:06 +0800

    add review mode

--- Games/types/Mafia/roles/cards/ControlPuppet.js
+++ Games/types/Mafia/roles/cards/ControlPuppet.js
@@ -14,6 +14,13 @@ module.exports = class ControlPuppet extends Card {
         
         let puppet = this.role.game.getPlayer(message.abilityTarget);
         message.sender = puppet;
+        message.parseForReview = this.parseForReview;
     }
 
+    parseForReview(message) {
+        let puppet = this.game.getPlayer(message.abilityTarget);
+
+        message.prefix = `controlling ${puppet.name}`
+        return message;
+    }
 }
