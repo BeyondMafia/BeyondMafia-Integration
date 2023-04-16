b8aa8f651539889b7b29d5666c577225ccedf1a1
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-16 18:32:08 +0800

    fix nomad

--- Games/types/Mafia/roles/Independent/Nomad.js
+++ Games/types/Mafia/roles/Independent/Nomad.js
@@ -6,7 +6,7 @@ module.exports = class Nomad extends Role {
         super("Nomad", player, data);
 
         this.alignment = "Independent";
-        this.cards = ["VillageCore", "WinIfAliveWhenTargetWin"];
+        this.cards = ["VillageCore", "ModifyAlignment"];
     }
 
 }
---
+++ Games/types/Mafia/roles/cards/ModifyAlignment.js
@@ -0,0 +1,33 @@
+const Card = require("../../Card");
+const { PRIORITY_MODIFY_ALIGNMENT } = require("../../const/Priority");
+
+module.exports = class ModifyAlignment extends Card {
+
+    constructor(role) {
+        super(role);
+        
+        this.winCount = "Village";
+
+        this.meetings = {
+            "Follow the ways of": {
+                states: ["Night"],
+                flags: ["voting"],
+                action: {
+                    priority: PRIORITY_MODIFY_ALIGNMENT,
+                    run: function () {
+                        let alignment = this.target.role.alignment;
+                        if (alignment == "Independent") {
+                            this.actor.queueAlert(`You follow ${this.target.name} but did not find a home.`)
+                            return;
+                        }
+
+                        this.actor.role.alignment = alignment;
+                        this.actor.queueAlert(`You follow ${this.target.name} and learn the ways of the ${alignment}.`);
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
