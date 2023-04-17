4135884870273188b2226534e584015df0471108
Peter Kaltzis <peterkaltzis@outlook.com>
2023-04-17 14:32:15 +1000

    upgraded insanity speech filter

--- Games/types/Mafia/effects/Insanity.js
+++ Games/types/Mafia/effects/Insanity.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const Effect = require("../Effect");
 const Random = require("../../../../lib/Random");
 
@@ -8,7 +9,7 @@ module.exports = class Insanity extends Effect {
     }
 
     speak(message) {
-        message.content = this.makeStrInsane(message.content);
+        message.content = this.makeStringInsane(message.content);
         message.parseForReview = this.parseForReview;
         message.modified = true;
     }
@@ -22,22 +23,33 @@ module.exports = class Insanity extends Effect {
         return message;
     }
 
-    makeStrInsane(str) {
-        var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var originalLength = str.length;
-
-        str = "";
-
-        for (let i = 0; i < originalLength; i++) {
-            let isSpace = Random.randFloat();
-
-            if (isSpace < 0.2)
-                str += " ";
-            else
-                str += chars[Random.randInt(0, chars.length - 1)];
+    makeStringInsane(string) {
+        // Generating the shuffled encryption key.
+        const unshuffledKey = "I can still feel it in my mind, watching my every move...".split("");
+        let secretKey = "";
+        while (unshuffledKey.length > 0) {
+            secretKey += unshuffledKey.splice(Random.randInt(0, unshuffledKey.length - 1), 1)[0];
         }
 
-        return str;
-    }
+        // Converting to 32 bytes used by AES.
+        const encryptionKey = Buffer.alloc(32);
+        encryptionKey.write(secretKey, "utf8");
 
+        // Encrypting message with AES.
+        const cipher = crypto.createCipheriv("aes-256-gcm", encryptionKey, crypto.randomBytes(16));
+        let encryptedString = cipher.update(string, "utf8", "base64");
+
+        // Adding some spaces, just to be safe! That's how encryption works right?
+        let index = 0;
+        let spaceProbabilities = [0, 0.01, 0.05, 0.20, 0.45, 0.70, 0.85, 0.93, 0.98, 0.99, 1.00];
+        for (let i=0; i<encryptedString.length; i++) {
+            if (Random.randFloat() < spaceProbabilities[index]) {
+                encryptedString = encryptedString.substring(0, i) + " " + encryptedString.substring(i);
+                index = 0;
+            } else {
+                index += 1;
+            }
+        }
+        return encryptedString;
+    }
 };
