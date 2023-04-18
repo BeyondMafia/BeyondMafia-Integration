2cfdc0ac0e11cfc51ec662b8a81a68e765d38890
r3ndd <r3ndd@users.noreply.github.com>
2023-04-18 09:54:20 -0700

    React build

--- react_main/build/precache-manifest.41710a6611135f7f1d41557b23fdbeb8.js
+++ react_main/build/precache-manifest.41710a6611135f7f1d41557b23fdbeb8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1664873d05c80d6b61cfa0daa2ad37b6",
+    "revision": "5553e90a1c9ef3d78a67b3427877c11b",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f669b6da.chunk.css"
   },
   {
-    "revision": "060b2c4a59cf1bdc0669",
+    "revision": "570a38d7e4a1707a953e",
     "url": "/static/css/main.3fd33a7e.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.82c08ca3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "060b2c4a59cf1bdc0669",
-    "url": "/static/js/main.3a797af1.chunk.js"
+    "revision": "570a38d7e4a1707a953e",
+    "url": "/static/js/main.58ad9bec.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/static/js/main.3a797af1.chunk.js.LICENSE.txt"
+    "url": "/static/js/main.58ad9bec.chunk.js.LICENSE.txt"
   },
   {
     "revision": "15c7af0c929ff2659c72",
