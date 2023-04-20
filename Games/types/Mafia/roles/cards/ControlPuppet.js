96e59932a26d37768b96184ddfcd6678c90612f9
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-20 20:48:46 +0800

    fix: vented player cannot see vented messages

--- Games/types/Mafia/roles/cards/ControlPuppet.js
+++ Games/types/Mafia/roles/cards/ControlPuppet.js
@@ -14,13 +14,18 @@ module.exports = class ControlPuppet extends Card {
         
         let puppet = this.role.game.getPlayer(message.abilityTarget);
         message.sender = puppet;
+
+        message.recipients = [];
+        for (let player of message.game.players)
+            if (player != puppet) 
+                message.recipients.push(player);
+
         message.parseForReview = this.parseForReview;
     }
 
     parseForReview(message) {
-        let puppet = this.game.getPlayer(message.abilityTarget);
-
-        message.prefix = `controlling ${puppet.name}`
+        message.recipients = message.versions["*"].recipients;
+        message.prefix = message.versions["*"].prefix;
         return message;
     }
 }
