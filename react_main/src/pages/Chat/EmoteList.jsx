ac5c5b0f80a439ab0b390086568fd1ce7eb030f9
PatakiGergo <pataki821@gmail.com>
2023-04-22 09:49:42 +0200

    Made the emote page look nicer
    Made the emote page look nicer

--- react_main/src/pages/Chat/EmoteList.jsx
+++ react_main/src/pages/Chat/EmoteList.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect } from "react";
 import { Emotes, emotify } from "../../components/Emotes";
+import "../../css/emotes.css";
 
 export default function EmoteList() {
-	useEffect(() => {
-		document.title = "Emotes | BeyondMafia";
-	}, []);
+  useEffect(() => {
+    document.title = "Emotes | BeyondMafia";
+  }, []);
 
-	return (
-		<div className="span-panel main legal">
-			<h1>List of Emotes {emotify(":doge:")}</h1>
-            <div>{getEmotes()}</div>
-        </div>
-	);
-    
+  return (
+    <div className="span-panel main legal">
+      <h1>List of Emotes {emotify(":doge:")}</h1>
+      <div>{getEmotes()}</div>
+    </div>
+  );
 }
 
 function getEmotes() {
-    const emotesList = Object.keys(Emotes);
-    return (
-        <ul>
-            {emotesList.map((emote) => (
-                <li key={emote}>
-                    {emote} {emotify(emote)}
-                </li>
-            ))}
-        </ul>
-    )
+  const emotesList = Object.keys(Emotes);
+  return (
+    <ul className="emote-list">
+      {emotesList.map((emote) => (
+        <li key={emote}>
+          {emote} {emotify(emote)}
+        </li>
+      ))}
+    </ul>
+  );
 }
