35bc5b2c1bf7a13bf655debb59476714a330ddd0
joyce <24848927+pikulet@users.noreply.github.com>
2023-04-22 11:01:53 +0800

    fix capitalisation of On

--- Games/types/Mafia/roles/Village/Leader.js
+++ Games/types/Mafia/roles/Village/Leader.js
@@ -5,7 +5,7 @@ module.exports = class Leader extends Role {
     constructor(player, data) {
         super("Leader", player, data);
         this.alignment = "Village";
-        this.cards = ["VillageCore", "WinWithVillage", "LeaderlessonDeath"];
+        this.cards = ["VillageCore", "WinWithVillage", "LeaderlessOnDeath"];
     }
 
 }
