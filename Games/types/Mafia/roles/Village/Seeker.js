c8bc939d5079af1dc615e714ff51d3543283e282
RH <104711111+R9H9@users.noreply.github.com>
2023-04-29 20:10:03 +1000

    Update and rename Agent.js to Seeker.js
    Signed-off-by: RH <104711111+R9H9@users.noreply.github.com>

--- Games/types/Mafia/roles/Village/Seeker.js
+++ Games/types/Mafia/roles/Village/Seeker.js
@@ -1,16 +1,16 @@
 const Role = require("../../Role");
 
-module.exports = class Agent extends Role {
+module.exports = class Seeker extends Role {
 
     constructor(player, data) {
-        super("Agent", player, data);
+        super("Seeker", player, data);
 
         this.alignment = "Village";
         this.cards = ["VillageCore", "WinWithVillage", "GuessAdversaryKill"];
         this.roleToGuess = "Spy";
         this.meetingMods = {
             "Guess Adversary": {
-                actionName: "Guess Spy"
+                actionName: "Guess Inquisitor"
             }
         };
     }
