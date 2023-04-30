fd44fbef4cd05a9aff7ce29ee30f92ca8bea5acd
pikulet <24848927+pikulet@users.noreply.github.com>
2023-04-30 08:38:56 +0800

    fix: sleepwalker only visits while dead (#728)

--- Games/types/Mafia/roles/cards/SleepWalk.js
+++ Games/types/Mafia/roles/cards/SleepWalk.js
@@ -1,6 +1,7 @@
 const Card = require("../../Card");
 const Random = require("../../../../../lib/Random");
 const Action = require("../../Action");
+const { PRIORITY_MESSAGE_GIVER_DEFAULT } = require("../../const/Priority");
 
 module.exports = class SleepWalk extends Card {
 
@@ -9,6 +10,10 @@ module.exports = class SleepWalk extends Card {
 
         this.listeners = {
             "state": function (stateInfo) {
+                if (!this.player.alive) {
+                    return
+                }
+
                 if (!stateInfo.name.match(/Night/)) {
                     return
                 }
@@ -20,6 +25,7 @@ module.exports = class SleepWalk extends Card {
                     actor: this.player,
                     target: target,
                     game: this.player.game,
+                    priority: PRIORITY_MESSAGE_GIVER_DEFAULT,
                     run: function() {}
                 })
 
