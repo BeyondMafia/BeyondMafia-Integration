f410d695e8c390b5e6c0051450efe579687bad81
pikulet <24848927+pikulet@users.noreply.github.com>
2023-05-03 03:26:20 +0000

    fix: santa message is consistent with other roles

--- Games/types/Mafia/roles/cards/GivePresents.js
+++ Games/types/Mafia/roles/cards/GivePresents.js
@@ -15,11 +15,12 @@ module.exports = class GivePresents extends Card {
                     priority: PRIORITY_ITEM_GIVER_DEFAULT,
                     run: function() {
                         let itemType = this.actor.role.data.itemType;
-                        if (itemType){
-                            let alert = `:sy3a: You have recieved ${(itemType === "Armor" ? itemType : "a " + itemType).toLowerCase()}!`
-                            this.target.holdItem(itemType);
-                            this.target.queueAlert(alert);
+                        if (!itemType) {
+                            return;
                         }
+
+                        this.target.holdItem(itemType);
+                        this.queueGetItemAlert(itemType);
                         delete this.actor.role.data.itemType;
 
                     }
