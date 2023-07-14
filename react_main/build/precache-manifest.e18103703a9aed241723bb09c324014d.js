fc929486452c5353378861b75a7b4914315b448e
minokr <136652649+minokr@users.noreply.github.com>
2023-07-14 16:35:46 -0400

    react rebuild (#891)

---
+++ react_main/build/precache-manifest.e18103703a9aed241723bb09c324014d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e234976c6e31c96f15de2ca55822421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2688f9af13dd9ab8a660",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "16d38c09ffcee69cafdf",
+    "url": "/static/css/main.7b1d45d5.chunk.css"
+  },
+  {
+    "revision": "2688f9af13dd9ab8a660",
+    "url": "/static/js/2.ce895316.chunk.js"
+  },
+  {
+    "revision": "19a9624e2a7a41d1463ce482aa86aa8e",
+    "url": "/static/js/2.ce895316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d38c09ffcee69cafdf",
+    "url": "/static/js/main.2d261dcb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2d261dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
