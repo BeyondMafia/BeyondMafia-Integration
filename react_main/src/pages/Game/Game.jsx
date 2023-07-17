c54cc882b2f77f231c3bcbf709a5c6ef1bbbe488
Mino <secondarymino@gmail.com>
2023-07-16 23:14:10 -0400

    State 0 is usually N0 or day start, include this

--- react_main/src/pages/Game/Game.jsx
+++ react_main/src/pages/Game/Game.jsx
@@ -1364,7 +1364,7 @@ function Message(props) {
     }
 
     if (player !== undefined) {
-        if (playerDead && props.stateViewing > 0 && stateMeetingDefined) {
+        if (playerDead && props.stateViewing > -1 && stateMeetingDefined) {
             contentClass += "dead";
         } else if (player.birthday !== undefined && areSameDay(Date.now(), player.birthday)) {
             contentClass += " party ";
