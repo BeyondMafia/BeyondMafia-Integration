8bbced682a25052ae5275e1382e92d1872f8d2a4
Mino <secondarymino@gmail.com>
2023-07-19 04:37:42 -0400

    react build to complete all changes from master and this

---
+++ react_main/build/precache-manifest.793593038b33437ac323702d7ff82495.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d94cf44374869972579638d28a2a3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5627a7e87a32f5baa08a",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "0c8183bd3cf029a3dd87",
+    "url": "/static/css/main.a2ca88fb.chunk.css"
+  },
+  {
+    "revision": "5627a7e87a32f5baa08a",
+    "url": "/static/js/2.2ff02770.chunk.js"
+  },
+  {
+    "revision": "19a9624e2a7a41d1463ce482aa86aa8e",
+    "url": "/static/js/2.2ff02770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8183bd3cf029a3dd87",
+    "url": "/static/js/main.d45c81ab.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d45c81ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
--- react_main/build/service-worker.js
+++ react_main/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.379c4a79abed3cf0049ea2b78548b3b3.js"
+  "/precache-manifest.793593038b33437ac323702d7ff82495.js"
 );
 
 self.addEventListener('message', (event) => {
