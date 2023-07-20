246e62e58d95ca6adee227d2b6d9f7efe0ad60f6
Mino <secondarymino@gmail.com>
2023-07-20 02:41:27 -0400

    Fresh react build

---
+++ react_main/build/precache-manifest.c9d12b26be836b860fc0d7cc0e1e3b40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da4126c2af3f07656eccf56d4c92ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15aeb3ce1b534409e208",
+    "url": "/static/css/2.93c90398.chunk.css"
+  },
+  {
+    "revision": "86f46c5f318bf3883047",
+    "url": "/static/css/main.39413180.chunk.css"
+  },
+  {
+    "revision": "15aeb3ce1b534409e208",
+    "url": "/static/js/2.0a5a9c37.chunk.js"
+  },
+  {
+    "revision": "19a9624e2a7a41d1463ce482aa86aa8e",
+    "url": "/static/js/2.0a5a9c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f46c5f318bf3883047",
+    "url": "/static/js/main.775253bb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.775253bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7af0c929ff2659c72",
+    "url": "/static/js/runtime-main.14957c3d.js"
+  }
+]);
